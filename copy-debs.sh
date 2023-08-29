66844a0559221762beb9e4355249c36de4ec1655
Your Name <you@example.com>
2023-08-29 18:12:59 +0200

    Copy File

---
+++ copy-debs.sh
@@ -0,0 +1,2 @@
+cp /rpool/lochert/idp-lochert/src/sonic-image-intel-instructions/sde/built-debs/bfnsdk_1.0.0_amd64.deb /rpool/lochert/sonic-buildimage
+cp /rpool/lochert/idp-lochert/src/sonic-image-intel-instructions/sde/built-debs/bfnplatform_1.0.0_amd64.deb /rpool/lochert/sonic-buildimage 
